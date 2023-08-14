62c287a48361169401f5a0f7b88aa0af4510bc1c
Caio <caiozs21@gmail.com>
2023-08-14 17:22:10 -0300

    fix: params activities user

--- src/routers/activities-router.ts
+++ src/routers/activities-router.ts
@@ -1,14 +1,14 @@
-import { Router } from 'express';
-import { getActivities, getActivitiesDay, getUserActivities, registerUserActivity } from '@/controllers';
-import { authenticateToken } from '@/middlewares';
+import { Router } from "express";
+import { getActivities, getActivitiesDay, getUserActivities, registerUserActivity } from "@/controllers";
+import { authenticateToken } from "@/middlewares";
 
 const activitiesRouter = Router();
 
 activitiesRouter
-  .all('/*', authenticateToken)
-  .get('/', getActivities)
-  .get('/user/:id', getUserActivities)
-  .get('/day', getActivitiesDay)
-  .post('/register', registerUserActivity);
+  .all("/*", authenticateToken)
+  .get("/", getActivities)
+  .get("/user/:id", getUserActivities)
+  .get("/day", getActivitiesDay)
+  .post("/register", registerUserActivity);
 
 export { activitiesRouter };
